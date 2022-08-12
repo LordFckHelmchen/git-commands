3f0b3a4c9bd634fcf07b38b25b2c2e2f29d65986
David J. Mack <MDC1RT@bosch.com>
2022-08-12 11:32:15 +0200

    build: Add "install" script for symlinks

---
+++ symlink_files.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ln -s "$HOME"/Git/git-commands/.bash_aliases "$HOME"/.
+ln -s "$HOME"/Git/git-commands/.bash_profile "$HOME"/.
+ln -s "$HOME"/Git/git-commands/.bashrc "$HOME"/.
+ln -s "$HOME"/Git/git-commands/.bash_completion "$HOME"/.
+
